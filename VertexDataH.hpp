f37544b5b2b8f65b77508e99c2848b0c467465dd
Ann <114284726+Anntasy@users.noreply.github.com>
2025-11-10 13:38:07 +0000

    Add files via upload

---
+++ VertexDataH.hpp
@@ -0,0 +1,29 @@
+# ifndef VERTEXDATA_HPP
+# define VERTEXDATA_HPP
+
+# include "h.hpp"
+
+
+class VertexData
+{
+public:
+	VertexData(std::size_t v_capacity, std::size_t v_step, std::size_t i_capacity, std::size_t i_step);
+	~VertexData();
+
+	void move_to_buffer();
+	void mark_buffer();
+
+	std::size_t v_size;
+	std::size_t i_size;
+	std::size_t v_capacity;
+	std::size_t i_capacity;
+	std::size_t v_step;
+	std::size_t i_step;
+
+	GLfloat *vertices;
+	GLuint *indices;
+
+};
+
+
+# endif
